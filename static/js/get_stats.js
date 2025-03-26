ea024537b25f0460f7980dd8dda865b3e55239ce
Camille Teicheira <cteicheira@gmail.com>
2025-03-26 12:21:03 -0700

    add some logging to stats gen

--- static/js/get_stats.js
+++ static/js/get_stats.js
@@ -3,13 +3,22 @@ const getWeather = require("./weather.js");
 const getPowerData = require("./powerinfo.js");
 
 const STATS_FILE = "/var/www/html/api/stats.json";
+const LOG = true;
 
 getWeather()
     .then((weatherData) => {
         const powerData = getPowerData();
 
-        const statsObject = { ...powerData, ...weatherData };
+        const statsObject = JSON.stringify(
+            { ...powerData, ...weatherData },
+            null,
+            2
+        );
 
-        fs.writeFileSync(STATS_FILE, JSON.stringify(statsObject, null, 2));
+        fs.writeFileSync(STATS_FILE, statsObject);
+        if (LOG) {
+            console.log("Stats written to", STATS_FILE);
+            console.log(statsObject);
+        }
     })
     .catch((err) => console.error(err));
