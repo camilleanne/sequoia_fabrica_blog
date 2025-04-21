29c46b691a314d4b814ca8829ad0e706b6355f39
Camille Teicheira <cteicheira@gmail.com>
2025-04-21 16:16:16 -0700

    update deploy script; add deploy workflow

--- utils/build_site.sh
+++ utils/build_site.sh
@@ -1,57 +1,83 @@
 #!/bin/bash
+set -e;
 # © 2023 Roel Roscam Abbing, released as AGPLv3
 # see https://www.gnu.org/licenses/agpl-3.0.html
 # Support your local low-tech magazine: https://solar.lowtechmagazine.com/donate/
-# Updated 2025 by Camille Teicheira
+# Updated 2025 by Camille Teicheira for Sequioa Fabrica
 
-now=`date`
-# baseURL="https://sequoiafabrica.org" #the URL of the website e.g. htttps://solar.lowtechmagazine.com/
-baseURL="http://localhost:9000"
-contentDir="$(pwd)/content" #the directory where your HUGO articles are e.g. /path/to/repo/solar_v2/content/
-repoDir="$(pwd)" #the full path to the repository
-outputDir="$(pwd)/output" # the directory where you export the site to.
-# python="/usr/bin/python3"
+# Default values
+baseURL="//localhost:9000" #the URL of the website e.g. https://solar.lowtechmagazine.com/
+repoDir="$(pwd)"
+contentDir="$repoDir/content"
+outputDir="$(pwd)/output"
 
-
-while getopts f flag
-do
-    case "${flag}" in
-        f) updated="forced rebuild";;
+# Parse named arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --baseURL*|--baseURL)
+        if [[ "$1" == *=* ]]; then baseURL="${1#*=}"; else shift; baseURL="$1"; fi;;
+        --repoDir*|--repoDir)
+            if [[ "$1" == *=* ]]; then repoDir="${1#*=}"; else shift; repoDir="$1"; fi
+        contentDir="$repoDir/content";;
+        --contentDir*|--contentDir)
+        if [[ "$1" == *=* ]]; then contentDir="${1#*=}"; else shift; contentDir="$1"; fi;;
+        --outputDir*|--outputDir)
+        if [[ "$1" == *=* ]]; then outputDir="${1#*=}"; else shift; outputDir="$1"; fi;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
+    shift
 done
 
-if [[ $updated != "forced rebuild" ]]; then
-        echo "Checking for update $now"
-        updated=$(git -C $repoDir pull origin main)
-fi
+echo "Building site with the following parameters:"
+echo "baseURL: $baseURL"
+echo "repoDir: $repoDir"
+echo "contentDir: $contentDir"
+echo "outputDir: $outputDir"
+echo ""
 
+now=`date`
+now_seconds=`date +%s`
 
-if  echo $updated | grep -q "Already up to date";
-then 
-        echo "Git up to date $now"
-else
-        echo "Git was not up to date"
-fi
-        mkdir -p $outputDir
+# # TODO: do I want to keep this?
 
-        echo $updated
-        echo "Rebuilding the site"
+# while getopts f flag
+# do
+#     case "${flag}" in
+#         f) updated="forced rebuild";;
+#     esac
+# done
 
-        cd $repoDir
+# if [[ $updated != "forced rebuild" ]]; then
+#         echo "Checking for update $now"
+#         updated=$(git -C $repoDir pull origin main)
+# fi
 
-        echo "Dithering new images"
-        # TODO: colorize requires updates to "categories" (still configured for lowtech mag)
-        python3 utils/dither_images.py -d $contentDir # --colorize
+# if  echo $updated | grep -q "Already up to date";
+# then
+#         echo "Git up to date $now"
+# else
+#         echo "Git was not up to date"
+# fi
+mkdir -p $outputDir
 
-        echo "Generating site"
-        hugo --baseURL $baseURL --destination $outputDir --ignoreCache --environment production
+echo $updated
+echo "Rebuilding the site"
 
-        echo "Calculating page sizes"
-        python3 utils/calculate_size.py --directory $outputDir --baseURL $baseURL
+cd $repoDir
 
-        echo "Removing original media from" $outputDir
-        python3 utils/clean_output.py --directory $outputDir
+echo "Dithering new images"
+# TODO: colorize requires updates to "categories" (still configured for lowtech mag)
+python3 utils/dither_images.py -d $contentDir # --colorize
 
-        after_seconds=`date +%s`
-        time_elapsed=$(( $after_seconds - $now_seconds ))
-        echo "Site regeneration took $time_elapsed seconds"
+echo "Generating site"
+hugo --baseURL $baseURL --destination $outputDir --ignoreCache --environment production --minify
+
+echo "Calculating page sizes"
+python3 utils/calculate_size.py --directory $outputDir --baseURL $baseURL
+
+echo "Removing original media from" $outputDir
+python3 utils/clean_output.py --directory $outputDir
+
+after_seconds=`date +%s`
+time_elapsed=$(( $after_seconds - $now_seconds ))
+echo "Site generation took $time_elapsed seconds"
